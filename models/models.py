bb9b929277ccbf2e2521127940e8fae8343c39d8
qfastok <denis.yakovlv@gmail.com>
2021-06-09 15:13:18 +0300

    Fixed bug

--- models/models.py
+++ models/models.py
@@ -9,12 +9,13 @@ class Picking(models.Model):
 
     def button_validate(self):
         for line in self.move_line_ids:
-            count = self.env['stock.move.line'].search_count([
-                ('date', '<=',
-                 datetime.combine(datetime.now(), time.max)),
-                ('date', '>=',
-                 datetime.combine(datetime.now(), time.min))])
-            if not line.lot_name:
-                line.lot_name = fields.datetime.now().strftime(
-                    '%y%m%d') + '/' + str(count).zfill(3)
+            if line.product_id.tracking == 'lot':
+                count = self.env['stock.move.line'].search_count([
+                    ('date', '<=',
+                     datetime.combine(datetime.now(), time.max)),
+                    ('date', '>=',
+                     datetime.combine(datetime.now(), time.min))])
+                if not line.lot_name:
+                    line.lot_name = datetime.now().strftime(
+                        '%y%m%d') + '/' + str(count).zfill(3)
         return super(Picking, self).button_validate()
